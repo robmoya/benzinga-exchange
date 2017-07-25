df32864523b34cf3048fdae1d1f052163afaa074
Rob <lastres.moya.53@gmail.com>
2017-07-24 21:39:59 -0500

    Initial Commit

---
+++ src/components/Greeter.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Greeter extends Component {
+    render() {
+        return(
+            <h4>Greeter</h4>
+        )
+    }
+}
+
+export default Greeter;
