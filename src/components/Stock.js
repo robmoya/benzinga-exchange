2dbf11b9a216077885ff42e50d975ab5a23e1bc8
Rob <lastres.moya.53@gmail.com>
2017-07-26 21:17:04 -0500

    buy and sell types

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react';
 
+let tradeT;
+
 class Stock extends Component {
+    getTradeType(e){
+        tradeT = e.target.name;
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        let tradeQ = this.refs.quantity.value;
+
+        if (tradeQ.length > 0) {
+            this.refs.quantity.value = '';
+            this.props.onAddTrade(tradeQ,tradeT);
+        } else {
+            alert("Please select a Quantity to sell or buy first")
+            this.refs.quantity.focus();
+        }
+    }
     render(){
         let {name,symbol,askPrice,bidPrice} = this.props;
         return(
@@ -18,10 +35,10 @@ class Stock extends Component {
                         </tr>
                     </tbody>
                 </table>
-                <form>
-                    <input type="text" placeholder="Enter Quantity"/>
-                    <button>Buy</button>
-                    <button>Sell</button>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <input type="text" placeholder="Enter Quantity" ref="quantity"/>
+                    <button name="buy" onClick={this.getTradeType}>Buy</button>
+                    <button name="sell" onClick={this.getTradeType}>Sell</button>
                 </form>
             </div>
         )
